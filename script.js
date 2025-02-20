666adcbc6017b5a48972fd3530ca046af8dc166c
Prishan_105546 <prishan.niwanjaya@axiatadigitallabs.com>
2025-02-20 17:02:55 +0530

    Update Script

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const script = document.currentScript;
 const url = new URL(script.src)
 const apiKey = url.searchParams.get("apiKey")
 
-// const baseUrl = 'https://bloom-engine.netlify.app/.netlify/functions/api'
-const baseUrl = 'http://localhost:8888/.netlify/functions/api'
+const baseUrl = 'https://bloom-engine.netlify.app/.netlify/functions/api'
+// const baseUrl = 'http://localhost:8888/.netlify/functions/api'
 
-function getInputData() {
+const getInputData = () => {
     const inputData = {};
     document.querySelectorAll('#bloom-container input[data-label], #bloom-container select[data-label]').forEach(input => {
         console.log(input.dataset.label)
@@ -15,54 +15,36 @@ function getInputData() {
     return inputData;
 }
 
-function getOutputLabels() {
-    return Array.from(document.querySelectorAll('#bloom-container #outputs [data-label]'))
+const getOutputLabels = () => {
+    return Array.from(document.querySelectorAll('#bloom-container [data-label]:not(input):not(select)'))
         .map(el => el.dataset.label);
 }
 
-fetch(`${baseUrl}/initialize`,
-    {
-        method: 'GET', // or 'POST', 'PUT', etc.
-        headers: {
-            'Content-Type': 'application/json',
-            'x-api-key': `${apiKey}`,
-        }
-    }
-)
-    .then(res => res.text())
-    .then(html => {
-        const container = document.getElementById('bloom-container');
-        container.innerHTML = html;
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector('#bloom-container form').addEventListener('submit', (event) => {
+        event.preventDefault()
 
         const req = {
-            inputs: {},
-            outputs: []
+            inputs: getInputData(),
+            outputs: getOutputLabels()
         }
 
-        document.querySelector('#bloom-container form').addEventListener('submit', (event) => {
-            event.preventDefault()
-            req.inputs = getInputData()
-            req.outputs = getOutputLabels()
-            let output = {}
+        let output = {}
 
-            fetch(`${baseUrl}/v3.0`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'x-api-key': `${apiKey}`,
-                },
-                body: JSON.stringify(req)
-            })
-                .then(response => response.json())
-                .then(data => {
-                    output = data.data
-                    console.log(output)
-                    Object.keys(output).forEach(result => {
-                        document.querySelector(`[data-label="${result}"]`).innerText = document.querySelector(`[data-label="${result}"]`).innerText.replace('{}', output[result])
-                    })
-                })
-                .catch(error => console.error('Error:', error));
-
-
+        fetch(`${baseUrl}/v3.0`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-api-key': `${apiKey}`,
+            },
+            body: JSON.stringify(req)
         })
-    });
+            .then(response => response.json())
+            .then(data => {
+                output = data.data
+                Object.keys(output).forEach(result => {
+                    document.querySelector(`[data-label="${result}"]`).innerText = output[result]
+                })
+            })
+    })
+});
